2d32dba11ceb34136a15e7514c857346e215a4f7
Artem Kashin <dev.tema@gmail.com>
2022-12-19 18:30:48 +0300

    feat: rps page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,10 @@ import '../styles/globals.css';
 import { ThemeProvider } from 'next-themes';
 
 import type { AppProps } from 'next/app';
-import Layout from '../components/Layout';
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class">
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <Component {...pageProps} />
     </ThemeProvider>
   );
 }
